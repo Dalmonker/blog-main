f586a4c1d4aa004f1443b3f5b995e8493602c4c3
Dalmonker <gavrilovichdmitry1@gmail.com>
2026-01-20 23:54:11 +0300

    +

--- templates/index.php
+++ templates/index.php
@@ -6,7 +6,6 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-<!--    <link rel="stylesheet" href=".././css/album.css">-->
     <title>Document</title>
 </head>
 <body>
